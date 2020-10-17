43f72946d9a02846003304e607c2a5dc65a0a599
Nico Smit <nico@limabean.co.za>
2020-10-17 23:42:11 +0200

    playing around with GCP GKE

---
+++ index.php
@@ -0,0 +1,11 @@
+<h1>nicosmit.com</h1>
+<p>Oh you thought this was a real website?</p>
+<p>Nope!</p>
+<p>My LinkedIn: <a href="https://www.linkedin.com/in/nicolaassmit/">linkedin.com/in/nicolaassmit</a></p>
+<p>One of my blogs: <a href="https://fantasticalmuse.com/">fantasticalmuse.com</a></p>
+
+<!--
+
+Docker image: gcr.io/nico-smit-personal/php-quickstart:v1
+
+-->
