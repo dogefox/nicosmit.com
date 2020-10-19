1ba8b588984f990efe6e3e6ec3aa662b57ae120c
Nico Smit <nico@limabean.co.za>
2020-10-19 09:42:50 +0200

    adding kubernetes files

--- index.php
+++ index.php
@@ -5,10 +5,9 @@ $domain =  $_SERVER['HTTP_HOST'];
     if(strpos($domain,'offgrid4x4')!==FALSE){
         header("Location: https://www.facebook.com/offgrid4x4/");
     }
-
+    echo "<h1>$domain</h1>";
 ?>
 
-<h1>nicosmit.com</h1>
 <p>Oh you thought this was a real website?</p>
 <p>Nope!</p>
 <p>My LinkedIn: <a href="https://www.linkedin.com/in/nicolaassmit/">linkedin.com/in/nicolaassmit</a></p>
